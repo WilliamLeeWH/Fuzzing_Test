044c58d5f216b1e5c76d4cd141fda0d13eef2b0d
WilliamLee <1412638411@qq.com>
2024-06-04 22:35:31 +0800

    path power scheduler

--- simple_fuzzer/schedule/PathPowerSchedule.py
+++ simple_fuzzer/schedule/PathPowerSchedule.py
@@ -1,7 +1,10 @@
-from typing import Dict, Sequence
+import math
 
+from typing import Dict, Sequence
 from schedule.PowerSchedule import PowerSchedule
 from utils.Seed import Seed
+from typing import Set
+from utils.Coverage import Location
 
 
 class PathPowerSchedule(PowerSchedule):
@@ -9,7 +12,22 @@ class PathPowerSchedule(PowerSchedule):
     def __init__(self) -> None:
         super().__init__()
         # TODO
+        self.path_freq = {}
+        self.alpla = 1
+
+    def update_path_freq(self, path: Set[Location]):
+        if path in self.path_freq:
+            self.path_freq[path] += 1
+        else:
+            self.path_freq[path] = 1
 
     def assign_energy(self, population: Sequence[Seed]) -> None:
         """Assign exponential energy inversely proportional to path frequency"""
         # TODO
+        for seed in population:
+            self.update_path_freq(seed.coverage)
+            path_freq = self.path_freq[seed.coverage] / sum(self.path_freq.values())
+            seed.energy = math.exp(-self.alpla * path_freq)
+        
+        
+
